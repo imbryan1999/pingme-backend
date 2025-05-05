3584f7580ba81a8f28f0c3e1233a51502dccc498
Hemant Shrestha <hemantshrsth@gmail.com>
2025-05-06 00:38:30 +0530

    socket connection

--- index.js
+++ index.js
@@ -2,16 +2,41 @@
 import app from "./app.js";
 import db from "./config/db_config.js"
 import routes from "./routes/user_route.js"
+import { Server } from "socket.io";
+import http from "http";
 
 // Set the port 
 const port = process.env.PORT || 6969;
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am Live :)")
+const server = http.createServer(app)
+
+// attach socket io to server
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 })
 
+// handle socket connection
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+  socket.on("newMessage", (data) => {
+    console.log("Received:", data);
+    io.emit("newMessage", data);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+})
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am Live :)");
+});
+
 // Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started on port ${port}`); 
 });
 
--- routes/main_home_route.js
+++ routes/main_home_route.js
@@ -4,5 +4,5 @@ import { getFriendList } from "../controller/friendlist_controller.js"
 const router = express.Router()
 
 router.get('/friendList', getFriendList)
-
+// https://youtu.be/_mR6bY-ndso?si=ypCpW20bCRfX73OM
 export default router
